ecfa39a0a2942b097cbd90206a7a10be3e248f28
My EVM <yo@myevm.org>
2025-06-13 22:59:53 +0000

    .

---
+++ src/types/Buybox.ts
@@ -0,0 +1,16 @@
+export interface BuyBox {
+  cities?: string[]; // Submitted by users
+  city?: string;     // Used when saving one per city
+  county?: string;
+  propertyType: string;
+  bedMin: number;
+  bathMin: number;
+  yearBuiltMin?: number;
+  sqftMin: number;
+  sqftMax?: number;
+  arvPercentMax?: number;
+  maxRehabCost?: number;
+  maxPrice?: number;
+  hoa?: boolean;
+  foundation?: string;
+}
