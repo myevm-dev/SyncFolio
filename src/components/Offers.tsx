3f8967e0dc3509071cc2fef0b580810671dbc77a
My EVM <yo@myevm.org>
2025-06-14 05:36:28 +0000

    .

--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -97,7 +97,7 @@ export default function Offers({ results, cashOnCashResults }: Props) {
                 }`}
                 disabled={isComingSoon}
               >
-                Accepted
+                Finalize
               </button>
             </div>
           </div>
