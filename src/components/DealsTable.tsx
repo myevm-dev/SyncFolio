d510a2baa6f18e5b04a8f3ea5b02a06c3ba92d98
My EVM <yo@myevm.org>
2025-06-22 18:49:58 +0000

    .

--- src/components/DealsTable.tsx
+++ src/components/DealsTable.tsx
@@ -404,7 +404,7 @@ const methods = ["unknown", "cash", "seller finance", "takeover", "hybrid"];
                 )}
 
                 {/* Dollar Icon */}
-                <button title="Finance Info">
+                <button title="Sell Lead">
                   <CurrencyDollarIcon className="w-5 h-5 text-green-500 hover:text-green-700 inline-block" />
                 </button>
 
